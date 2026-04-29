01fdc96b757431f139ec2a2a76bf23413e58eaee
Wordy <wordy@radiusred.ai>
2026-04-29 10:18:13 +0000

    docs(handoff): include latest RAD-637 commits in apply script

--- handoff/apply-rad-637.sh
+++ handoff/apply-rad-637.sh
@@ -7,7 +7,7 @@ if [[ -f handoff/RAD-637.bundle ]]; then
   git fetch handoff/RAD-637.bundle HEAD
 fi
 
-git cherry-pick 79ec946 797a9df 081094e 383c637
+git cherry-pick 79ec946 797a9df 081094e 383c637 e06be26 2ed75af c7e3f38
 
 echo "Done. Next:"
 echo "  git push origin HEAD"
